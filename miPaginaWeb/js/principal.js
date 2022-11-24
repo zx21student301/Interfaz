6c2bd206b9b9003d199b28fd26b3a35081bedb09
Eric Knuchel <zx21student301@colegiovalledelmiro.es>
2022-11-24 23:41:47 +0100

    24/11

--- miPaginaWeb/js/principal.js
+++ miPaginaWeb/js/principal.js
@@ -1,46 +1,116 @@
 onload = principal
 
 function principal(){
+    //recoge en un array todos los elementos de la class "ver"
     let ver = document.getElementsByClassName("ver");
-    let id = 1;
+    let idV = 1;
 
+    //por cada elemento del array ver, le añade los siguientes atributos en la pagina html
     for (v of ver){
-        v.setAttribute("onclick","cargarVer("+id+")");
+        v.setAttribute("onclick","cargarVer("+idV+")");
         v.setAttribute("abierto","false");
-        v.setAttribute("id",id);
-        id++;
+        v.setAttribute("id","v"+idV);
+        idV++;
     }
 
-    let ocultos = document.getElementsByClassName("ocul");
+    let menos = document.getElementsByClassName("menos");
+    let idM = 1;
 
-    id = 1;
+    //por cada elemento del array menos, le añade los siguientes atributos en la pagina html
+    for (m of menos){
+        m.setAttribute("onclick","cargarVer("+idM+")");
+        m.setAttribute("abierto","false");
+        m.setAttribute("id","m"+idM);
+        idM++;
+    }
 
-    for (o of ocultos){
-        o.setAttribute("id",id);
-        id++;
+    let cards = document.getElementsByClassName("card");
+
+    //Bucle que recorre el numero de tarjetas que hay y a los elementos de la class "ocul"+i los pone en diplay none
+    for(i=1;i < cards.length+1; i++){
+        let ocultos = document.getElementsByClassName("ocul"+i);
+        for (o of ocultos){
+            o.setAttribute("style","display:none");
+        }
+    }
+
+    //variable para diferenciar el card que se encuentra dentro de otro
+    let matrizVetos = document.getElementById("matrizVetos");
+    matrizVetos.setAttribute("onclick","mostrarCardMatrizVetos()");
+
+    //recoge el elmento de la class "contacto" y le añade un atributo
+    let contacto = document.getElementById("contacto");
+    contacto.setAttribute("onclick","verForm()");
+
+    //crea el formulario en el div "formulario" y lo deja en display none
+    crearForm();
+}
+
+//funcion que se ejecuta cuando se hace click sobre el span con class "ver"
+function cargarVer(id){
+    //Guarda en variables todos los elementos o atributos que se van a usar en la funcion
+    let ver = document.getElementById("v"+id);
+    let img = document.getElementById("divImgCard"+id);
+    let imgA = document.getElementById("imgAmpliada"+id);
+    let card = document.getElementById("row"+id);
+    let menos = document.getElementById("m"+id);
+
+    let ocultos = document.getElementsByClassName("ocul"+id);
+
+    let abierto = ver.getAttribute("abierto");
+
+    //recoge el atributo abierto y comprueba su contenido
+    if(abierto == "false"){
+
+        for (o of ocultos){
+            o.setAttribute("style","display:block;");
+        }
+
+        card.setAttribute("style","text-align:center;");
+
+        img.setAttribute("style","visibility:hidden");
+        img.setAttribute("class","col-md-3");
+
+        imgA.setAttribute("style","display:block;");
+
+        menos.setAttribute("abierto","true");
+        menos.setAttribute("style","display:block");
+
+        ver.setAttribute("abierto","true");
+        ver.innerHTML = "Ver menos";
+
+    }else if(abierto == "true"){
+
+        for (o of ocultos){
+            o.setAttribute("style","display:none");
+        }
+
+        card.setAttribute("style","text-align:left");
+
+        img.setAttribute("style","visibility:visible");
+        img.setAttribute("class","col-md-2");
+
+        imgA.setAttribute("style","display:none;");
+
+        menos.setAttribute("abierto","false");
+        menos.setAttribute("style","display:none");
+
+        ver.setAttribute("abierto","false");
+        ver.innerHTML = "Ver más";
     }
 }
 
-function cargarVer(id){
-    let ver = document.getElementById(id);
-    let ocultos = document.getElementsByClassName("ocul");
-    let abierto = ver.getAttribute("abierto");
-    let img = document.getElementById("divImgCard");
+//funcion para abrir y cerrar el card que se encuentra dentro de otro
+function mostrarCardMatrizVetos(){
+    
+}
 
-    if(abierto == "false"){
-        for (o of ocultos){
-            o.setAttribute("style","display:block")
-        }
-        img.setAttribute("class","col-md-5");
-        ver.setAttribute("abierto","true");
-        ver.innerHTML = "Ver menos"
+//funcion para crear el formulario cuando se cargue la pagina
+function crearForm(){
 
-    }else if(abierto == "true"){
-        for (o of ocultos){
-            o.setAttribute("style","display:none")
-        }
-        img.setAttribute("class","col-md-2");
-        ver.setAttribute("abierto","false");
-        ver.innerHTML = "Ver más"
-    }
+}
+
+//funcion para ver o dejar de ver el formulario
+function verForm(){
+
 }
